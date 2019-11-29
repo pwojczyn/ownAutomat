6b7fb44f8f02412ffb9e44d88eed0bb083a2e233
pwojczyn <piotr.wojczynski@gmail.com>
2019-11-29 09:02:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var staticFolder = 'public';
+app.use(express.static(staticFolder));
+
+app.get('/', (req, res) => {
+    res.send('ownAutomat work !');
+});
+
+app.get('/test', function (req, res) {
+  res.send('test command! ownAutmat');
+});
+
+app.get('/info', function(req, res){
+  res.sendFile('./' + staticFolder + '/info.html', { root: __dirname });
+}); 
+
+app.listen(3000, function () {
+  console.log('ownAutomat app listening on port 3000!');
+});
